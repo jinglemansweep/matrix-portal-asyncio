2f916ba7b669cbb90e0a06a8c72ec86be3a36fe1
Louis King <jinglemansweep@gmail.com>
2022-11-12 18:52:30 +0000

    Tweaks for Interstate 75

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,34 +1,45 @@
 import asyncio
 import board
 import busio
+import displayio
+import framebufferio
 import keypad
-
-from adafruit_matrixportal.matrix import Matrix
-from adafruit_matrixportal.network import Network
-from adafruit_lis3dh import LIS3DH_I2C
+import rgbmatrix
+#from adafruit_matrixportal.matrix import Matrix
+#from adafruit_matrixportal.network import Network
+#from adafruit_lis3dh import LIS3DH_I2C
 from rtc import RTC
 
 from app.utils import matrix_rotation, parse_timestamp
 
-BIT_DEPTH = 6
-NTP_ENABLE = True
+BIT_DEPTH = 5
+FPS_TARGET = 60
+NTP_ENABLE = False
 NTP_INTERVAL = 60 * 60  # 1h
-ASYNCIO_LOOP_DELAY = 0.02  # secs
+ASYNCIO_LOOP_DELAY = 0.005  # secs
 
+displayio.release_displays()
 
 class Manager:
     def __init__(self, theme, debug=False):
         print(f"MANAGER::INIT theme={theme} debug={debug}")
         # RGB Matrix
-        self.matrix = Matrix(bit_depth=BIT_DEPTH)
+        # self.matrix = Matrix(bit_depth=BIT_DEPTH)
+        self.matrix = rgbmatrix.RGBMatrix(
+            width=64, height=64, bit_depth=BIT_DEPTH,
+            rgb_pins=[board.R0, board.G0, board.B0, board.R1, board.G1, board.B1],
+            addr_pins=[board.ROW_A, board.ROW_B, board.ROW_C, board.ROW_D, board.ROW_E],
+            clock_pin=board.CLK, latch_pin=board.LAT, output_enable_pin=board.OE
+        )
         # Accelerometer
-        self.accelerometer = LIS3DH_I2C(busio.I2C(board.SCL, board.SDA), address=0x19)
-        _ = self.accelerometer.acceleration  # drain startup readings
+        # self.accelerometer = LIS3DH_I2C(busio.I2C(board.SCL, board.SDA), address=0x19)
+        # _ = self.accelerometer.acceleration  # drain startup readings
         # Display Buffer
-        self.display = self.matrix.display
-        self.display.rotation = matrix_rotation(self.accelerometer)
+        self.display = framebufferio.FramebufferDisplay(self.matrix, auto_refresh=False)
+        # self.display = self.matrix.display
+        # self.display.rotation = matrix_rotation(self.accelerometer)
         # Networking
-        self.network = Network(status_neopixel=board.NEOPIXEL, debug=debug)
+        #self.network = Network(status_neopixel=board.NEOPIXEL, debug=debug)
         # Theme
         self.theme = theme(display=self.display)
         # GPIO Buttons
@@ -62,13 +73,20 @@ class Manager:
                     self.last_pressed = key_number
                 await asyncio.sleep(0)
 
+    async def refresh_display(self):
+        self.display.refresh(minimum_frames_per_second=0, target_frames_per_second=FPS_TARGET)
+
     async def loop(self):
         print(f"MANAGER::LOOP")
         if NTP_ENABLE:
             asyncio.create_task(self.ntp_update())
-        asyncio.create_task(self.check_gpio_buttons())
+        # asyncio.create_task(self.check_gpio_buttons())
         await asyncio.create_task(self.theme.setup())
+        print('theme setup')
         while True:
             await asyncio.create_task(self.theme.loop(button=self.last_pressed))
             self.last_pressed = None
-            await asyncio.sleep(ASYNCIO_LOOP_DELAY)
+            await asyncio.create_task(self.refresh_display())
+            #self.display.refresh(target_frames_per_second=FPS_TARGET, minimum_frames_per_second=0)
+            
+
